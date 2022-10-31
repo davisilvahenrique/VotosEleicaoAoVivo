6944b5d9be25ccf4e02a9a52b54c3f7a2b106dc4
Davi Henrique <104919646+davisilvahenrique@users.noreply.github.com>
2022-10-31 08:42:32 -0300

    Update Eleicoes.py

--- Eleicoes.py
+++ Eleicoes.py
@@ -2,26 +2,41 @@ import requests
 import json
 import pandas as pd
 import time
+import os
+
+# WHILE UTILIZADO PARA REPETIR O PROCESSO
 
 while True:
+
+    # SISTEMA DE REQUEST DE INFORMAÇÕES DO SITE
+
     data = requests.get(
-        'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+        'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
     json_data = json.loads(data.content)
+
+    # CRIANDO LISTAS PARA GUARDAR INFORMAÇÕES
 
     candidato = []
     votos = []
     porcentagem = []
     porcentagemtotal = json_data['pst']
 
+    # ADICIONANDO INFORMAÇÕES AS LISTAS
+
     for informacoes in json_data['cand']:
-        
         candidato.append(informacoes['nm'])
         votos.append(informacoes['vap'])
         porcentagem.append(informacoes['pvap'])
 
+    # CRIAÇÃO DE TABELA
+
     df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
-                            'Candidato','N Votos','Porcentagem'])
-    print("----------------------------------------------")
-    print("Votos apurados =",porcentagemtotal,"%")
-    print(df_eleicao)
-    time.sleep(15)
+        'Candidato', 'N Votos', 'Porcentagem'])
+
+    # PRINT DE TODO O RESULTADO
+
+    os.system('cls')                                    # LIMPA O CONSOLE ANTES DE PRINTAR UM NOVO RESULTADO
+    print("Votos apurados =", porcentagemtotal, "%")    # PRINT DA PORCENTAGEM TOTAL DE VOTOS
+    print("")                                           # PULA LINHA
+    print(df_eleicao)                                   # PRINT DA TABELA
+    time.sleep(10)                                      # A CADA 10 SEGUNDO RETORNA PARA O WHILE TRUE
